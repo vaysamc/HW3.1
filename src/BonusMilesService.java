5c963b5bf0af4cc4770ac6ada634021510197d34
vaysamc <vasak7503@gmail.com>
2024-02-08 19:00:35 +0300

    mainBon commit

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,8 +1,8 @@
 public class BonusMilesService {
-    public int calculate(int price){
-  int result;
-  result = price/20;
-          return result;
+    public int calculate(int price) {
+        int result;
+        result = price / 20;
+        return result;
 
     }
 }
